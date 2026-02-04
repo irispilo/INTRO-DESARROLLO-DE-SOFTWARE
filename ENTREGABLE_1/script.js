d80bb519ce9d196015d6fc4445f16f0127541953
Iris <sucely.pilo@ufm.edu>
2026-02-04 06:24:54 -0600

    greeting added

--- ENTREGABLE_1/script.js
+++ ENTREGABLE_1/script.js
@@ -1,3 +1,17 @@
 function modoOscuro() {
 document.body.classList.toggle("oscuro");
 }
+
+
+  const hora = new Date().getHours();
+  let mensaje = "";
+
+  if (hora >= 6 && hora < 12) {
+    mensaje = "¡Buenos días!";
+  } else if (hora >= 12 && hora < 18) {
+    mensaje = "¡Buenas tardes!";
+  } else {
+    mensaje = "¡Buenas noches!";
+  }
+
+  document.getElementById("saludo").textContent = mensaje;
