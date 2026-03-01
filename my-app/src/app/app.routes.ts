1198ada51efd1fcd9d193687831d17cd4d8ada2e
Iris <sucely.pilo@ufm.edu>
2026-02-28 22:26:32 -0600

    Added 3 routes: header, listas, and tablas

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from '@angular/router';
+import { Header } from './components/header/header';
+import { Listas } from './components/listas/listas';
+import { Tablas } from './components/tablas/tablas';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+{path: "header", component: Header},
+{path: "listas", component: Listas},
+{path: "tablas", component: Tablas},
+
+
+
+];
 
 
 
