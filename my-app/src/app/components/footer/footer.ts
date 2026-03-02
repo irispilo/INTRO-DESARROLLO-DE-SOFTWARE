ce5504587f4387a90b01c2b076dbc458ed38ff66
Iris <sucely.pilo@ufm.edu>
2026-03-01 22:00:10 -0600

    Added custom initials pipe in footer and used titlecase (header) and date (footer) pipes

--- my-app/src/app/components/footer/footer.ts
+++ my-app/src/app/components/footer/footer.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { InitialsPipe } from '../../initials-pipe';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, InitialsPipe],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  today: Date = new Date();
 
 }
